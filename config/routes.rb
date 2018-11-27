c1b159d10105652b34b475923e6b362e21fa97f7
StefanWoerner <mail@stefan-woerner.com>
2018-11-27 11:21:59 +0100

    Controller und Models done

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,16 @@
 Rails.application.routes.draw do
+  get 'deals/index'
+  get 'deals/show'
+  get 'deals/create'
+  get 'deals/edit'
+  get 'deals/update'
+  get 'deals/destroy'
+  get 'restaurants/index'
+  get 'restaurants/show'
+  get 'restaurants/new'
+  get 'restaurants/create'
+  get 'restaurants/edit'
+  get 'restaurants/update'
   devise_for :users
   root to: 'pages#home'
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
