1cdca8d8fc5ce359b0cfb96e75476b6efc4249a2
Eike Feld <eike.feld@gmx.de>
2018-11-28 16:55:05 +0100

    deals controller + showpage

--- app/controllers/deals_controller.rb
+++ app/controllers/deals_controller.rb
@@ -1,4 +1,11 @@
 class DealsController < ApplicationController
   skip_before_action :authenticate_user!, only: :index
+  def index
+    @deals = Deal.all
+  end
 
+  def show
+    @deal = Deal.find(params[:id])
+    @deals = Deal.all
+  end
 end
