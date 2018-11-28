302bdf9ff20a7e656e5652662b4d819c63a873e0
Eike Feld <eike.feld@gmx.de>
2018-11-28 11:47:05 +0100

    fixed controller/index/show

--- app/controllers/restaurants_controller.rb
+++ app/controllers/restaurants_controller.rb
@@ -1,21 +1,21 @@
 class RestaurantsController < ApplicationController
   skip_before_action :authenticate_user!, only: :index
   def index
-    @restaurants = Restaurant.all
-    @restaurants = @restaurants.where(cuisine: params[:cuisine]) if params[:cuisine].present?
-    @restaurants = @restaurants.where(distance: params[:distance]) if params[:distance].present?
-    @restaurants = @restaurants.where(price_category: params[:price_category]) if params[:price_category].present?
-    @restaurants = @restaurants.where(food_type: params[:food_type]) if params[:food_type].present?
-    @restaurants = @restaurants.where(type_of_deal: params[:type_of_deal]) if params[:type_of_deal].present?
-    
+    @restaurant = Restaurant.all
+    @restaurant = @restaurant.where(cuisine: params[:cuisine]) if params[:cuisine].present?
+    @restaurant = @restaurant.where(distance: params[:distance]) if params[:distance].present?
+    @restaurant = @restaurant.where(price_category: params[:price_category]) if params[:price_category].present?
+    @restaurant = @restaurant.where(food_type: params[:food_type]) if params[:food_type].present?
+    @restaurant = @restaurant.where(type_of_deal: params[:type_of_deal]) if params[:type_of_deal].present?
+
     @restaurants_geo = @restaurant.near(params[:location], 3)
     @markers = @restaurants_geo.map do |restaurant|
-
-    {
-      lng: restaurant.longitude,
-      lat: restaurant.latitude,
-      infoWindow: { content: render_to_string(partial: "/restaurants/map_window", locals: { restaurant: restaurant }) }
+      {
+        lng: restaurant.longitude,
+        lat: restaurant.latitude,
+        infoWindow: { content: render_to_string(partial: "/restaurants/map_window", locals: { restaurant: restaurant }) }
       }
+    end
   end
 
   def show
@@ -30,36 +30,36 @@ class RestaurantsController < ApplicationController
     @restaurant = Restaurant.find(params[:id])
   end
 
-def new
-  @restaurant = Restaurant.new
-end
-
-def create
-  @restaurant = Restaurant.new(restaurant_params)
-  @restaurant.user = current_user
-  authorize @restaurant
-  if @restaurant.save
-    redirect_to restaurants_path
-  else
-    render new_restaurant_path
+  def new
+    @restaurant = Restaurant.new
   end
-end
 
-def edit
-  @restaurant = Restaurant.find(params[:id])
-  authorize @restaurant
-end
+  def create
+    @restaurant = Restaurant.new(restaurant_params)
+    @restaurant.user = current_user
+    authorize @restaurant
+    if @restaurant.save
+      redirect_to restaurants_path
+    else
+      render new_restaurant_path
+    end
+  end
 
-def update
-  @restaurant = Restaurant.find(params[:id])
-  authorize @restaurant
-  @restaurant.update(restaurant_params)
-  redirect_to restaurants_path
-end
+  def edit
+    @restaurant = Restaurant.find(params[:id])
+    authorize @restaurant
+  end
 
-private
+  def update
+    @restaurant = Restaurant.find(params[:id])
+    authorize @restaurant
+    @restaurant.update(restaurant_params)
+    redirect_to restaurants_path
+  end
 
-def restaurant_params
-  params.require(:restaurant).permit(:name, :location, :cuisine, :photo)
-end
+  private
+
+  def restaurant_params
+    params.require(:restaurant).permit(:name, :location, :cuisine, :photo)
+  end
 end
