4cc6691b99bfc0163d252d2a414cb2769675c26f
StefanWoerner <mail@stefan-woerner.com>
2018-11-28 20:46:43 +0100

    changes

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -1,3 +1,8 @@
 //= require rails-ujs
 //= require_tree .
 
+n =  new Date();
+y = n.getFullYear();
+m = n.getMonth() + 1;
+d = n.getDate();
+document.getElementById("date").innerHTML = d + "." + m + "." + y;
