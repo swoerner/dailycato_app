a0c268a55bf24004a688e84d5fafc95ba5a67f9f
Dylan Mertens <dylanjmertens@gmail.com>
2018-12-03 11:26:05 +0100

    Seeds & filter button

--- db/seeds.rb
+++ db/seeds.rb
@@ -52,9 +52,10 @@ def search_restaurants_details(array_of_ids)
       cuisine: response.parse["categories"][0]["title"],
       location: response.parse["location"]["address1"],
       zip_code: response.parse["zip_code"],
-      photo: response.parse["image_url"]
-      )
-    if response.parse["hours"]
+      photo: response.parse["image_url"],
+      photos: response.parse["photos"],
+      ),
+    if response.parse["hours"] && r
       days = %w(monday tuesday wednesday thursday friday saturday sunday)
       response.parse["hours"].first['open'].each do |hour|
         day = days[hour['day']]
@@ -69,23 +70,5 @@ results = search_restaurants_details(search('', 'berlin'))
 
 # Review.create!(
 #   restaurant_id: r.id,
-#   text: business["text"]
-#   )
-
-# )
-
-Deal.create!(name: "Brokkolicremesuppe", description: "mit gerösteten Mandeln und frischen Kräutern", food_type: "modern european" , price: 4, restaurant: Restaurant.first)
-Deal.create!(name: "Vegane Karottensuppe", description: "mit Ingwer, Sojamilch und karamellisierter roter Bete", food_type: "modern european" , price: 6, restaurant: Restaurant.first)
-Deal.create!(name: "Bunte Gemüsesuppe", description: "mit Wiener Würstchen, Croûtons und Petersilie", food_type: "modern european" , price: 5, restaurant: Restaurant.first)
-Deal.create!(name: "Buchweizen-Hackfleischtopf", description: "mit Paprika und Koriander", food_type: "modern european" , price: 3, restaurant: Restaurant.first)
-Deal.create!(name: "Thailändische grüne Currysuppe", description: "mit geflügelstreifen, asiatischem Gemüse und Koriander", food_type: "modern european" , price: 4, restaurant: Restaurant.first)
-
-Deal.create!(name: "Schnitzel", description: "mit Pommes Frites und Preiselbeeren", food_type: "modern european" , price: 5, restaurant: Restaurant.second)
-Deal.create!(name: "Jägerschnitzel", description: "in Pilzrahmsauce und Kroketten", food_type: "modern european" , price: 5, restaurant: Restaurant.second)
-Deal.create!(name: "Zigeunerschnitzel", description: "mit Pommes Frites", food_type: "modern european" , price: 5, restaurant: Restaurant.second)
-
-Deal.create!(name: "Spaghetti Carbonara", description: "mit Speck in Käsesahnesauce", food_type: "modern european" , price: 4, restaurant: Restaurant.third)
-Deal.create!(name: "Spaghetti Bolognese", description: "mit Fleischbällchen und Tomatensauce", food_type: "modern european" , price: 4, restaurant: Restaurant.third)
-Deal.create!(name: "Pizza Hawaii", description: "mit Schinken und Ananas", food_type: "modern european" , price: 4, restaurant: Restaurant.third)
-Deal.create!(name: "Pizza Frutti de Mare", description: "mit Scampis und Meeresfrüchten", food_type: "modern european" , price: 4, restaurant: Restaurant.third)
-Deal.create!(name: "Pizza Mageritha", description: "mit Käse und Tomatensauce", food_type: "modern european" , price: 4, restaurant: Restaurant.third)
+#   text: business["id"]["text"]
+#   text: response.parse   )
