203f1511a0c27c31c972216a0e665bc51c3ca7cc
Dylan Mertens <dylanjmertens@gmail.com>
2018-12-04 17:27:43 +0100

    yes

--- app/javascript/packs/form.js
+++ app/javascript/packs/form.js
@@ -1,5 +1,7 @@
 a = document.getElementById('booking_deal_id')
 b = document.getElementById('priceselect')
+c = document.getElementById('amount').value
+d = document.getElementById('total-price')
 
 a.addEventListener('change', function(){
   Array.from(b.children).forEach(function(x){
@@ -8,3 +10,14 @@ a.addEventListener('change', function(){
     }
   })
 })
+
+var price = document.getElementById('priceselect')
+var x = document.getElementById('amount').value
+var total_price = document.getElementById('total-price')
+
+function calcTotalPrice(x)
+{
+document.getElementById('total-price').value = price * x;
+}
+
+
