952f7da8d0d5896dbf8075400d466a67e15025b0
Minh Pham <mpham@naver.com>
2018-12-06 16:37:09 +0100

    Continue work on og image

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -6,7 +6,9 @@ before_action :configure_permitted_parameters, if: :devise_controller?
     # For additional fields in app/views/devise/registrations/new.html.erb
     devise_parameter_sanitizer.permit(:sign_up, keys: [:first_name, :last_name])
   end
+
   def default_url_options
-  { host: ENV["HOST"] || "localhost:3000" }
-end
+   { host: ENV["HOST"] || "localhost:3000" }
+  end
+
 end
